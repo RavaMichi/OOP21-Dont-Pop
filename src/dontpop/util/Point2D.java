46bb814529dbea320b84f7c25eda8b240e815395
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-03-10 16:50:55 +0100

    create class Point2D

---
+++ src/dontpop/util/Point2D.java
@@ -0,0 +1,13 @@
+package dontpop.util;
+
+public class Point2D {
+
+	private double x;
+	private double y;
+	
+	public Point2D(final double x, final double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+}
