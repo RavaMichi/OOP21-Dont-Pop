c77e6c71d11bbb69cf3c934a0252fd27ea1a9091
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-03-10 18:06:52 +0100

    add constructor and setter/getter for position

--- src/game/model/AbstractGameObject.java
+++ src/game/model/AbstractGameObject.java
@@ -1,5 +1,20 @@
 package game.model;
 
-public abstract class AbstractGameObject {
+import game.util.Point2D;
 
+public abstract class AbstractGameObject {
+	
+	private Point2D position;
+	
+	public AbstractGameObject(Point2D position) {
+		this.position = position;
+	}
+	
+	public Point2D getPosition() {
+		return this.position;
+	}
+	
+	public void setPosition(Point2D newPosition) {
+		this.position = newPosition;
+	}
 }
