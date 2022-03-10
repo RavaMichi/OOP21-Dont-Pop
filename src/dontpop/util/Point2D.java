91a6f72866ff82cf1c4c5a261d3280a10a77afdd
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-03-10 16:57:36 +0100

    add basic vector operations

--- src/dontpop/util/Point2D.java
+++ src/dontpop/util/Point2D.java
@@ -1,5 +1,7 @@
 package dontpop.util;
 
+import progettoOOP.util.Point2D;
+
 public class Point2D {
 
 	private double x;
@@ -24,5 +26,25 @@ public class Point2D {
 	
 	public void setY(double y) {
 		this.y = y;
+	}
+	
+	public void add(final Point2D point) {
+		this.x += point.getX();
+		this.y += point.getY();
+	}
+	
+	public void sub(final Point2D point) {
+		this.x -= point.getX();
+		this.y -= point.getY();
+	}
+	
+	public void mul(final double scalar) {
+		this.x *= scalar;
+		this.y *= scalar;
+	}
+	
+	public void div(final double scalar) {
+		this.x /= scalar;
+		this.y /= scalar;
 	}
 }
