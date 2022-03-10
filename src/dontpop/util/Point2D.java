02243fa583011e0e4785f9440067df7185c81f84
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-03-10 16:54:43 +0100

    add getters and setters to Point2D

--- src/dontpop/util/Point2D.java
+++ src/dontpop/util/Point2D.java
@@ -10,4 +10,19 @@ public class Point2D {
 		this.y = y;
 	}
 	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
 }
