9390bea047f15b3538d409effb1b72be075f46e5
Francesco Valentini - francesco.valentin11@studio.unibo.it <francesco.valentin11@studio.unibo.it>
2022-03-15 09:42:03 +0100

    Created GameObject class & declared methods instantiate & destroy

---
+++ src/game/controller/GameEngine.java
@@ -0,0 +1,18 @@
+package game.controller;
+
+import game.model.AbstractGameObject;
+
+/* 
+ * GameEngine is the class that makes the game work
+*/
+
+public class GameEngine {
+	
+	public void instantiate(AbstractGameObject obj) {
+		
+	}
+	
+	public void destroy(AbstractGameObject obj) {
+		
+	}
+}
