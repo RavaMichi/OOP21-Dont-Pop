3a29fa636b730910c375b91afb72cd520a499646
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-03-21 16:52:23 +0100

    add object reference and rotate function in ImageRenderer

--- src/game/graphics/ImageRenderer.java
+++ src/game/graphics/ImageRenderer.java
@@ -1,13 +1,19 @@
 package game.graphics;
 
+import game.model.AbstractGameObject;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 /**
  * Renderer of an image
  */
 public class ImageRenderer implements Renderer {
 
 	private Image img;
+	private String imgPath;
+	private final AbstractGameObject obj;
 	private double size;
 	/**
 	 * @param imgPath
@@ -15,18 +21,24 @@ public class ImageRenderer implements Renderer {
 	 * @param rotation in degrees
 	 * Creates a new Renderer with image of imgPath and in-game width of size rotated by rotation angle
 	 */
-	public ImageRenderer(final String imgPath, double size, double rotation) {
-		this.img = new Image(imgPath, false);
+	public ImageRenderer(final AbstractGameObject obj, final String imgPath, double size, double rotation) {
+		this.obj = obj;
+		this.imgPath = imgPath;
+		this.img = new Image(imgPath);
 		this.size = size;
 		this.rotate(rotation);
 	}
 	
 	@Override
 	public void paint(GraphicsContext gc) {
-		// TODO Auto-generated method stub
+        gc.drawImage(img, size, size);
 	}
 	
 	public void rotate(final double degrees) {
-		
+		ImageView iv = new ImageView(new Image(imgPath));
+		iv.setRotate(degrees);
+		SnapshotParameters param = new SnapshotParameters();
+		param.setFill(Color.TRANSPARENT);
+		this.img = iv.snapshot(param, null);
 	}
 }
