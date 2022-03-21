744cd8dc2dddb69fb2df183e1b467899b7d8b104
Francesco Valentini <fv26012001@gmail.com>
2022-03-22 00:19:18 +0100

    Add JavaDoc comments to ScoreManager and ScoreScene

--- src/game/graphics/ScoreManager.java
+++ src/game/graphics/ScoreManager.java
@@ -1,7 +1,13 @@
 package game.graphics;
 
+/**
+ * ScoreManager calculates the current score.
+ */
 public class ScoreManager {
 
+    /**
+     * Creates & initializes this class.
+     */
     public ScoreManager() {
         // TODO Auto-generated constructor stub
     }
--- src/game/graphics/ScoreScene.java
+++ src/game/graphics/ScoreScene.java
@@ -1,7 +1,13 @@
 package game.graphics;
 
+/**
+ * ScoreScene displays a GUI showing your current score.
+ */
 public class ScoreScene {
 
+    /**
+     * Creates & initializes this class.
+     */
     public ScoreScene() {
         // TODO Auto-generated constructor stub
     }
