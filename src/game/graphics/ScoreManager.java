5cf3a416608accf5f0ab1de310d43d2a7b993d78
Francesco Valentini <fv26012001@gmail.com>
2022-03-22 00:33:41 +0100

    Added methods menu() and get() to ScoreManager and ScoreScene, respectively. Added a few fields.

--- src/game/graphics/ScoreManager.java
+++ src/game/graphics/ScoreManager.java
@@ -5,11 +5,20 @@ package game.graphics;
  */
 public class ScoreManager {
 
+    private int score;
+    private int multiplier;
+
     /**
      * Creates & initializes this class.
      */
     public ScoreManager() {
-        // TODO Auto-generated constructor stub
+        this.multiplier = 1;
     }
 
+    /**
+     * TODO: WRITE A JAVADOC FOR THIS CLASS.
+     */
+    public void menu() {
+
+    }
 }
--- src/game/graphics/ScoreScene.java
+++ src/game/graphics/ScoreScene.java
@@ -5,11 +5,23 @@ package game.graphics;
  */
 public class ScoreScene {
 
+    private ScoreManager scoreManager;
+    
     /**
      * Creates & initializes this class.
      */
     public ScoreScene() {
-        // TODO Auto-generated constructor stub
+        this.scoreManager = new ScoreManager();
+    }
+    
+    /**
+     * @return this ScoreScene
+     * Gets this ScoreScene
+     */
+    public ScoreScene get() {
+        //DETAILS NEED TO GET DEFINED BETTER
+        //DO I NEED A NEW SCENE CLASS?
+        return this;
     }
 
 }
