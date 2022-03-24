d86c43d53f83244df3b35cb82802d76b2f16c047
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-03-24 10:10:05 +0100

    add getMouseWorldPosition and modify constructor in GameScene

--- src/game/graphics/GameScene.java
+++ src/game/graphics/GameScene.java
@@ -3,10 +3,13 @@ package game.graphics;
 import java.util.List;
 
 import game.model.AbstractGameObject;
+import game.util.Point2D;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 /**
  * Represents the Scene where to display the actual game objects.
  * It is managed by the GameEngine
@@ -15,15 +18,26 @@ public class GameScene {
     
 	private final Scene scene;
 	private Canvas canvas;
+	private Point2D mousePosition = new Point2D(0.5,0.5);
 	/**
 	 * @param sizeX
 	 * @param sizeY
 	 * Creates a new GameScene of size (in pixels) sizeX*sizeY
 	 */
-	public GameScene(int sizeX, int sizeY) {
+	public GameScene(int size) {
 		Group gr = new Group();
-		this.canvas = new Canvas(sizeX, sizeY);
-		gr.getChildren().add(gr);
+		this.canvas = new Canvas(size, size);
+		gr.getChildren().add(this.canvas);
+		
+		//Updates the mouse position
+		gr.setOnMouseMoved(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				mousePosition.setX(event.getSceneX()/size);
+				mousePosition.setY(event.getSceneY()/size);
+			}
+		});
+		
 		this.scene = new Scene(gr);
 	}
 	/**
@@ -37,6 +51,12 @@ public class GameScene {
 	 */
 	public GraphicsContext getGraphics() {
 		return this.canvas.getGraphicsContext2D();
+	}
+	/**
+	 * @return the in-game coordinates of the mouse
+	 */
+	public Point2D getMouseWorldPosition() {
+		return this.mousePosition; 
 	}
 	/**
 	 * @param objects - List of game objects
