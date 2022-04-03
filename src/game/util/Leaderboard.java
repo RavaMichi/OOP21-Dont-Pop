b80f2d041c20b06b23dad28f1e79f2f890b9b86a
Francesco Valentini <fv26012001@gmail.com>
2022-04-03 20:34:29 +0200

    Make a few variables final, change for loop to while loop, make exceptions more specific, make ArrayList type of first field as List (following warnings)

--- src/game/util/Leaderboard.java
+++ src/game/util/Leaderboard.java
@@ -2,13 +2,15 @@ package game.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 /**
- * Class used to store the scores of the best players
+ * Class used to store the scores of the best players.
  */
 
 //prima salva la lunghezza della classifica, poi gli elementi
@@ -16,34 +18,37 @@ public class Leaderboard {
 
 	private static final int RANKING_LENGTH = 5;
 	
-	private ArrayList<Pair<String,Integer>> ranking = new ArrayList<>();
+	private List<Pair<String,Integer>> ranking = new ArrayList<>();
 	private final String savePath;
 	
 	public Leaderboard(final String savePath) {
 		this.savePath = savePath;
 	}
+	
 	/**
 	 * @return the current ranking
 	 */
 	public List<Pair<String,Integer>> getRanking() {
 		return List.copyOf(this.ranking);
 	}
+	
 	/**
 	 * @param playerName
 	 * @param score
 	 * Adds to the ranking the player, if the score is in the top 5 scores
 	 */
-	public void addToRanking(String playerName, int score) {
-		var entry = new Pair<String,Integer>(playerName, score);
+	public void addToRanking(final String playerName, final int score) {
+		final var entry = new Pair<String,Integer>(playerName, score);
 		if (this.ranking.isEmpty()) {
 			this.ranking.add(entry);
 		} else {
 			//sorting during insertion
 			int index = 0;
-			for (; index < this.ranking.size(); index++) {
+			while (index < this.ranking.size()) {
 				if (score > this.ranking.get(index).get2()) {
 					break;
 				}
+				index++;
 			}
 			this.ranking.add(index, entry);
 			
@@ -55,23 +60,29 @@ public class Leaderboard {
 	
 	@SuppressWarnings("unchecked")
 	/**
-	 * Loads the ranking saved in the savefile
+	 * Loads the ranking saved in the savefile.
 	 */
 	public void load() {
 		try (var ois = new ObjectInputStream(new FileInputStream(new File(this.savePath)))) {
 			this.ranking = (ArrayList<Pair<String,Integer>>)ois.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} catch (ClassNotFoundException e3) {
+			e3.printStackTrace();
 		}
 	}
 	/**
-	 * Saves the current ranking in the savefile
+	 * Saves the current ranking in the savefile.
 	 */
 	public void save() {
 		try (var oos = new ObjectOutputStream(new FileOutputStream(new File(this.savePath)))) {
 			oos.writeObject(this.ranking);
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
 		}
 	}
 }
