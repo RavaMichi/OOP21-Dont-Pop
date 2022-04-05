ab0b69dd4152e530f4afabdd6f24944673088322
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-05 18:04:23 +0200

    create new class CircleRenderer

---
+++ src/game/graphics/CircleRenderer.java
@@ -0,0 +1,13 @@
+package game.graphics;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class CircleRenderer implements Renderer {
+
+	@Override
+	public void paint(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
