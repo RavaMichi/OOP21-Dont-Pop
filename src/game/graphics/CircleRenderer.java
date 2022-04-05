6d2900ac666719457502f9a5b72b21f26e38c21f
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-05 18:10:10 +0200

    add constructor to CircleRenderer

--- src/game/graphics/CircleRenderer.java
+++ src/game/graphics/CircleRenderer.java
@@ -1,9 +1,29 @@
 package game.graphics;
 
+import game.model.AbstractGameObject;
+import game.util.Point2D;
 import javafx.scene.canvas.GraphicsContext;
-
+import javafx.scene.paint.Color;
+/**
+ * Renderer of a circle
+ */
 public class CircleRenderer implements Renderer {
 
+	private AbstractGameObject parent;
+	private double radius;
+	private Color color;
+	/**
+	 * @param parent - the GameObject
+	 * @param radius - the circle radius
+	 * @param color - the circle color
+	 * Creates a new Renderer at parent position, representing a circle of radius radius and color color
+	 */
+	public CircleRenderer(final AbstractGameObject parent, double radius, Color color) {
+		this.parent = parent;
+		this.radius = radius;
+		this.color = color;
+	}
+	
 	@Override
 	public void paint(GraphicsContext gc) {
 		// TODO Auto-generated method stub
