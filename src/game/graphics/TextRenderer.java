d6c47e9c0a2928f6c45fdce1e7da290cd7130d52
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-06 17:23:36 +0200

    crate class TextRenderer

---
+++ src/game/graphics/TextRenderer.java
@@ -0,0 +1,19 @@
+package game.graphics;
+
+import game.model.AbstractGameObject;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.Font;
+
+public class TextRenderer implements Renderer {
+
+	private Font font;
+	private String text;
+	private AbstractGameObject parent;
+	
+	@Override
+	public void paint(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
