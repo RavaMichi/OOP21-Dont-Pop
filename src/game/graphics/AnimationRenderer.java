e11b03aa93ce7396f635129e8162ded1e992fb7a
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-07 08:54:29 +0200

    implement render method, add javadoc comments

--- src/game/graphics/AnimationRenderer.java
+++ src/game/graphics/AnimationRenderer.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import game.engine.GameEngine;
 import javafx.scene.canvas.GraphicsContext;
-
+/**
+ * Renderer of an animation - a sequence of image renderers
+ */
 public class AnimationRenderer implements Renderer {
 
 	private final List<ImageRenderer> frames;
@@ -12,6 +14,14 @@ public class AnimationRenderer implements Renderer {
 	private boolean loop;
 	private final GameEngine ge;
 	
+	private double timer = 0;
+	private int index = 0;
+	/**
+	 * Creates a new Renderer representing an sequence of images.
+	 * @param images to display in order
+	 * @param speed - frames per seconds
+	 * @param loop - enables the looping of the animation
+	 */
 	public AnimationRenderer(final List<ImageRenderer> images, double speed, boolean loop) {
 		if (images == null || images.size() == 0) throw new IllegalArgumentException("images can't be null or empty.");
 		this.frames = images;
@@ -22,8 +32,27 @@ public class AnimationRenderer implements Renderer {
 	
 	@Override
 	public void paint(GraphicsContext gc) {
-		// TODO Auto-generated method stub
-
+		frames.get(index).paint(gc);
+		incIndex();
+	}
+	/**
+	 * Used to update the sequence
+	 */
+	private void incIndex() {
+		if (this.index < this.frames.size()) {
+			//update timer
+			this.timer += ge.getDeltaTime();
+			//index increment condition
+			if (this.timer >= this.animationTime) {
+				this.timer -= this.animationTime;
+				
+				this.index++;
+				if (this.loop && this.index > this.frames.size()) {
+					//loop
+					this.index = 0;
+				}
+			}
+		}
 	}
 
 }
