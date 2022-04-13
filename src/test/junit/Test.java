8e6ae231b77194965a099b912421603b40b3205e
michele.ravaioli3 <michele.ravaioli3@CEZ-SALAP-PORT9.campusfc.dir.unibo.it>
2022-04-13 12:17:45 +0200

    add test of circle collider

--- src/test/junit/Test.java
+++ src/test/junit/Test.java
@@ -1,7 +1,48 @@
 package test.junit;
+import static org.junit.Assert.*;
 
-public class ColliderTest {
+import game.collider.CircleCollider;
+import game.collider.Collider;
+import game.model.AbstractGameObject;
+import game.util.Point2D;
 
+public class Test {
 	
+	private AbstractGameObject player;
+	
+	private void initPlayer() {
+		//creates a game object at the center
+		player = new AbstractGameObject(Point2D.of(0.5, 0.5), null, null) {
+			@Override
+			public void update() {
+				//moves horizontally
+				this.getPosition().add(Point2D.of(0.1, 0));
+			}
+			
+		};
+		//circle of radius 0.2
+		player.setCollider(new CircleCollider(player, 0.1));
+	}
+	
+	@org.junit.Test
+	public void testCircleCollider() {
+		initPlayer();
+		var circle = new AbstractGameObject(Point2D.of(1, 0.5), null, null) {
+			@Override
+			public void update() {
+			}
+		};
+		circle.setCollider(new CircleCollider(circle, 0.05));
+		
+		//they don't touch
+		assertFalse(circle.getCollider().checkCollision((CircleCollider)player.getCollider()));
+		player.update();
+		player.update();
+		assertFalse(circle.getCollider().checkCollision((CircleCollider)player.getCollider()));
+		player.update();
+		player.update();
+		//they touch
+		assertTrue(circle.getCollider().checkCollision((CircleCollider)player.getCollider()));
+	}
 	
 }
