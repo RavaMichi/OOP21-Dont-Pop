42ab500eddb9f71cd9fa3c16a5a64bf70106f20f
Francesco Valentini - francesco.valentin11@studio.unibo.it <francesco.valentin11@studio.unibo.it>
2022-04-13 12:17:51 +0200

    Add JavaDoc comments

--- src/game/engine/GameApplication.java
+++ src/game/engine/GameApplication.java
@@ -1,6 +1,6 @@
 package game.engine;
-//import java.awt.Dimension;
 
+//import java.awt.Dimension;
 //import java.awt.Toolkit;
 
 import game.ui.GameScene;
@@ -21,33 +21,51 @@ public class GameApplication extends Application {
 	String playerName;
 	private Leaderboard leaderboard;
 	private ScoreManager scoremanager;
-	public final static int screenSize;// usare la percentuale dello schermo non valori da 0 a 1: 0.n * size; dove n è
-									// la percentuale dello schermo. ergo ho i valori da 0 a 1
+	public final static int screenSize;	// usare la percentuale dello schermo non valori da 0 a 1: 0.n * size; dove n è
+										// la percentuale dello schermo. ergo ho i valori da 0 a 1
 	private Stage primaryStage;
 //	ScoreCalc scoreCalc = new ScoreCalc();
-		
-		
+
+	/**
+	 * Static initializer: gets screen size.
+	 */
 	static {
 		Rectangle2D screenBounds = Screen.getPrimary().getBounds();
 		screenSize = (int) Math.min(screenBounds.getWidth(), screenBounds.getHeight());
 	}
 
-	
+	/**
+	 * Set player name to the String passed as argument.
+	 * @param playerName
+	 */
 	public void setPlayerName(final String playerName) {
 		this.playerName = playerName;
 	}
 	
 	// prende un int che va da zero a 1
+	/**
+	 * Convert pizel size to percentage size.
+	 * @param num
+	 * @return percentage size
+	 */
 	public static double convertToDouble(final int num) {// deve essere compreso tra 0 e n e lo devo trasformare in 0-1
 		return num / screenSize;// ritorna la posizione percentuale rispetto allo schermo. esempio gli passo 350 e la risoluzione è 700 lui mi tira fuori 0.5 (50% della lunghezza dello
 		// schermo )
 	}
 
 	// fa l'opposto di quello sopra
+	/**
+	 * Convert percentage size to pixel size.
+	 * @param num
+	 * @return pixel size
+	 */
 	public static int convertToInt(final double num) {
 		return (int) (num * screenSize);
 	}
 
+	/**
+	 * Start GUI, then launch the menu.
+	 */
 	public void start(final Stage primaryStage) { // public void start(final Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.leaderboard = new Leaderboard(this.playerName);
@@ -58,6 +76,9 @@ public class GameApplication extends Application {
 			
 	}
 
+	/**
+	 * Launch menu GUI.
+	 */
 	public void menu() {
 		//menumanager non servirà a un cazzo, poi andrà tolto
 		MenuManager menumanager = new MenuManager();		//manca la classe, aspetto per modificare il cosrtuttore
@@ -65,7 +86,9 @@ public class GameApplication extends Application {
 		this.setSceneM(menuscene.get());					//manca la classe, aspett il nome del metodo
 	}
 
-
+	/**
+	 * Launch game GUI (run the game).
+	 */
 	public void game() {
 		GameScene gamescene = new GameScene(screenSize);
 		GameEngine gameEngine = new GameEngine(this, gamescene);
@@ -74,6 +97,10 @@ public class GameApplication extends Application {
 
 	}
 
+	/**
+	 * Launch score GUI (view score ranking).
+	 * @param score
+	 */
 	public void score(final int score) {
 		ScoreManager scoremanager = new ScoreManager(this.playerName, score, this.leaderboard, this);// prende score e il nome  player e leaderboard dal game engine e aggiungere al costruttpre il nome del player
 		ScoreScene scoreScene = new ScoreScene(this.scoremanager, screenSize);//aggiungere il manager
@@ -82,6 +109,10 @@ public class GameApplication extends Application {
 	}
 
 	// SETTA LA SCENA CHE GLI PASSO AL THREAD DEL JAVA FX
+	/**
+	 * Set JavaFX Thread scene to the scene passed as argument.
+	 * @param scene
+	 */
 	void setSceneM(Scene scene) {
 		Platform.runLater(() -> {
 			primaryStage.setScene(scene);
@@ -89,7 +120,10 @@ public class GameApplication extends Application {
 		});
 	}
 	
-	
+	/**
+	 * Main function: launch the program.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
