d84c6af7e575c72b3f5ae0aecae207d81b7d5006
Francesco Valentini <fv26012001@gmail.com>
2022-04-16 17:44:12 +0200

    Improve menuscene

--- src/game/ui/MenuSceneController.java
+++ src/game/ui/MenuSceneController.java
@@ -2,4 +2,13 @@ package game.ui;
 
 public class MenuSceneController {
 
+	public void play() {
+		
+	}
+	
+	public void menu() {
+		
+	}
+	
+	//get player name box text and set it as player name in game application
 }
