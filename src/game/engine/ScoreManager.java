d3671eba4fc941da49da5d434cba68d70f696f0d
Francesco Valentini <46844696+Fv2601@users.noreply.github.com>
2022-04-19 17:44:23 +0200

    Update ScoreManager.java

--- src/game/engine/ScoreManager.java
+++ src/game/engine/ScoreManager.java
@@ -3,7 +3,6 @@ package game.engine;
 import java.util.List;
 
 import game.util.Leaderboard;
-import game.util.ScoreCalc;
 import game.util.Pair;
 
 /**
@@ -16,9 +15,14 @@ public class ScoreManager {
 	private String playerName;
 	private Leaderboard leaderboard;
 	private GameApplication application;
+	private boolean readOnly;
 	
     /**
      * Creates & initializes this class.
+     * @param playerName
+     * @param score
+     * @param leaderboard
+     * @param application
      */
     public ScoreManager(final String playerName, final int score, final Leaderboard leaderboard, final GameApplication application) {
     	this.score = score;
@@ -31,6 +35,19 @@ public class ScoreManager {
     	//save in file after adding current record
     	this.leaderboard.save();
     }
+    
+    /**
+     * This constructor is useful when all you want to do is view current leaderboard, without editing it.
+     * @param leaderboard
+     * @param application
+     */
+    public ScoreManager(final Leaderboard leaderboard, final GameApplication application) {
+    	this.readOnly = true;
+    	this.leaderboard = leaderboard;
+    	this.application = application;
+    	
+    	this.leaderboard.load();
+    }
 
     /**
      * Gets a copy of the ranking list inside Leaderboard.
@@ -40,20 +57,45 @@ public class ScoreManager {
     	return this.leaderboard.getRanking();
     }
     
+    /**
+     * Gets current player name.
+     * @return player name
+     */
     public String getPlayerName() {
     	return this.playerName;
     }
     
+    /**
+     * Gets current player's score (at gameover).
+     * @return score
+     */
     public int getScore() {
     	return this.score;
     }
     
     /**
-     * Returns to menu.
+     * Gets current player's rank (at gameover).
+     * @return rank
      */
-    public void menu() {
+    public String getRank() {
+    	return String.valueOf(this.leaderboard.getRank(this.getPlayerName(), this.getScore()));
+    }
+    
+    /**
+     * Returns to menu.
+     * @throws Exception 
+     */
+    public void menu() throws Exception {
     	//TODO: call menu method of application
-    	//ScoreScene will call this method
+    	this.application.menu();
+    }
+    
+    /**
+     * Checks whether the leaderboard should be read-only or not.
+     * @return true if read-only, false if leaderboard shall be edited
+     */
+    public boolean isReadOnly() {
+    	return this.readOnly;
     }
     
 }
