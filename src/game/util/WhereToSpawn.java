5686d943f150a7738cb916299b68686c645582f5
Manuel Tartagni <manuel.tartagni@studio.unibo.it>
2022-04-19 18:44:04 +0200

    changes in wheretospawn

--- src/game/util/WhereToSpawn.java
+++ src/game/util/WhereToSpawn.java
@@ -42,18 +42,18 @@ public class WhereToSpawn { //RITORNA UN POINT 2D IN CUI FAR SPAWNARE IL NOSTRO 
 		public  Point2D getEnemySpawnPoint(int side) {
 			
 			RandomInt randomInt= new RandomInt();
-			int sideOfSpawn= this.getRandomSide();
-			double randomNumber=randomInt.getRandomInt(0,100) / 100 ;
+			//int sideOfSpawn= this.getRandomSide();
+			double randomNumber=(double)randomInt.getRandomInt(0,100) / 100 ;
 			//SPAWNANO FUORI FDAL GAMEBOARD CON UNA DIFFERENZA DI double n= 0.2 (sia in positivo che in negativo)
 		
 			
-			if (sideOfSpawn == SideOfSpawn.WEST.ordinal() ) {
+			if (side == 1 ) {
 				return new Point2D(-0.2,randomNumber);
-			} else if ( sideOfSpawn == SideOfSpawn.SUD.ordinal() ) {
+			} else if ( side == 2 ) {
 				return new Point2D(randomNumber,1.2);
-			} else if ( sideOfSpawn == SideOfSpawn.EAST.ordinal() ) {
+			} else if ( side == 3 ) {
 				return new Point2D(1.2 , randomNumber);
-			} else if (sideOfSpawn == SideOfSpawn.NORD.ordinal() ) {
+			} else if (side == 4 ) {
 				return new Point2D(randomNumber,-0.2);
 			}
 			return new Point2D(0.2,-0.2); //IN CASO DI ERRORE SPAWN IN (0.2,-0.2)
@@ -75,9 +75,7 @@ public class WhereToSpawn { //RITORNA UN POINT 2D IN CUI FAR SPAWNARE IL NOSTRO 
 		
 		
 		public int getThornballRandomSide() {
-			RandomInt randomInt= new RandomInt();
-			int side = randomInt.getRandomInt(1,2);
-			return side;
+			return 1 + new RandomInt().getRandomInt(0, 1)*2;//1 o 3
 		}
 		
 public  Point2D getThornballSpawnPoint(int side) {
