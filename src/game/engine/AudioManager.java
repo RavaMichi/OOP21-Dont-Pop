e95593584c0c61fc0c1a74bf3f935031079fc439
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-20 22:06:23 +0200

    implement play for sounds and musics, implement stop for sounds and musics

--- src/game/engine/AudioManager.java
+++ src/game/engine/AudioManager.java
@@ -12,8 +12,8 @@ import javafx.scene.media.MediaPlayer;
  */
 public class AudioManager {
 	
-	private static Map<Music, MediaPlayer> musics;
-	private static Map<Sound, AudioClip> sounds;
+	private static final Map<Music, MediaPlayer> MUSICS = new HashMap<>();
+	private static final Map<Sound, AudioClip> SOUNDS = new HashMap<>();
 	
 	/**
 	 * Each element of this enum represents a music, playable by the method playMusic.
@@ -24,7 +24,7 @@ public class AudioManager {
 		
 		private Music(String path) {
 			Media media = new Media(getResPath(path));
-			musics.put(this, new MediaPlayer(media));
+			MUSICS.put(this, new MediaPlayer(media));
 		}
 	}
 	/**
@@ -35,15 +35,62 @@ public class AudioManager {
 		POP("");
 		
 		private Sound(String path) {
-			sounds.put(this, new AudioClip(getResPath(path)));
+			SOUNDS.put(this, new AudioClip(getResPath(path)));
 		}
 	}
-	
+	/**
+	 * Converts a resource path to a URI path, for Audio loading
+	 * @param path
+	 * @return correct path
+	 */
 	private static String getResPath(String path) {
 		return AudioManager.class.getClassLoader().getResource(path).toExternalForm();
 	}
 	
-	public void playSound() {
-		
+	private Map<Music, MediaPlayer> musics;
+	private Map<Sound, AudioClip> sounds;
+	
+	public AudioManager() {
+		this.musics = Map.copyOf(MUSICS);
+		this.sounds = Map.copyOf(SOUNDS);
+	}
+	
+	/**
+	 * Plays a sound using given volume. Volume is a number between 0.0 (muted) and 1.0 (full volume)
+	 * @param sound
+	 * @param volume
+	 */
+	public void playSound(final Sound sound, double volume) {
+		this.sounds.get(sound).play(volume);
+	}
+	/**
+	 * Plays a music using given volume. Volume is a number between 0.0 (muted) and 1.0 (full volume).
+	 * An AudioManager can play only one music at time.
+	 * @param music
+	 * @param volume
+	 */
+	public void playMusic(final Music music, double volume) {
+		stopMusics();
+		this.musics.get(music).setVolume(volume);
+		this.musics.get(music).play();
+	}
+	/**
+	 * Stops all the playing sounds
+	 */
+	public void stopSounds() {
+		this.sounds.forEach((s, a) -> a.stop());
+	}
+	/**
+	 * Stops all the playing musics
+	 */
+	public void stopMusics() {
+		this.musics.forEach((s, m) -> m.stop());
+	}
+	/**
+	 * Stops every audio playing
+	 */
+	public void stopAll() {
+		stopSounds();
+		stopMusics();
 	}
 }
