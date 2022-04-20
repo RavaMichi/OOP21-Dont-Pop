d8b21932cd29d79d1488586e74bb9263ebc0f7d0
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-20 22:48:14 +0200

    add audio manager to game engine

--- src/game/engine/GameEngine.java
+++ src/game/engine/GameEngine.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import game.util.Point2D;
 import game.collider.CircleCollider;
+import game.engine.AudioManager.Sound;
 import game.ui.GameScene;
 import game.model.*;
 import game.util.ScoreCalc;
@@ -20,6 +21,7 @@ public class GameEngine implements Runnable {
     private double gameTime;	//game time, starting from 0
     private final PlayerObj player;
     private final SpawnManager spawnManager;
+    private final AudioManager audioManager;
     private final ScoreCalc scoreCalc;
     private final GameScene gameScene;
     private final GameApplication application;
@@ -72,7 +74,7 @@ public class GameEngine implements Runnable {
         });
         this.scoreDisplay = new ScoreDisplayObj(new Point2D(SCORE_POS_X, SCORE_POS_Y), AbstractGameObject.ObjectType.SCORE, this);
         this.spawnManager = new SpawnManager(this);
-        
+        this.audioManager = new AudioManager();
         //likely add fps in future
     }
 
@@ -81,6 +83,9 @@ public class GameEngine implements Runnable {
      * @throws Exception 
      */
     public void startGameLoop() throws Exception {
+    	//start music
+    	this.audioManager.playMusic(AudioManager.Music.BALOON_GROOVE, 0.6);
+    	//start loop
         while (executeLoop) {
             //interval between "frames"
             final long startTime = System.currentTimeMillis();
@@ -360,6 +365,14 @@ public class GameEngine implements Runnable {
         renderList.add(this.scoreDisplay);
         
         this.gameScene.render(renderList);
+	}
+	/**
+	 * Plays a sound using given volume
+	 * @param sound
+	 * @param volume
+	 */
+	public void play(final Sound sound, double volume) {
+		this.audioManager.playSound(sound, volume);
 	}
 }
 
