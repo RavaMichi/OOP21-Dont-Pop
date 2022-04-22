279030015c8303ed951497e7a4102bf93486e5c9
IlManu0 <93584082+IlManu0@users.noreply.github.com>
2022-04-22 17:56:15 +0200

    Update HowToPlayScene.java

--- src/game/ui/HowToPlayScene.java
+++ src/game/ui/HowToPlayScene.java
@@ -23,7 +23,7 @@ public class HowToPlayScene {
 	 */
 	public HowToPlayScene(final GameApplication application, final int screenSize) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/game/fxml/howtoplay.fxml"));
+		loader.setLocation(getClass().getResource("/game/fxml/howtoplayscene.fxml"));
 
 		// controller created here
 		HowToPlaySceneController controller = new HowToPlaySceneController(application);
