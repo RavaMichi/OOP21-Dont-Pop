fec8f4adaaa15e04008eb735a52104c41b67a321
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-24 22:46:51 +0200

    fix Leaderboard test

--- src/test/junit/LeaderboardTest.java
+++ src/test/junit/LeaderboardTest.java
@@ -17,16 +17,16 @@ import java.util.List;
 public class LeaderboardTest {
 
 	private Leaderboard leaderboard;
-	private List<Pair<String, Integer>> list;
+	//private List<Pair<String, Integer>> list;
 	
-	@Before
-	public void init() {
+	private void init() {
 		this.leaderboard = new Leaderboard("src/test/junit/.save-test");
-		this.list = new ArrayList<Pair<String, Integer>>();
+		//this.list = new ArrayList<Pair<String, Integer>>();
 	}
-	
+	/*
 	@Test
 	public void testAddToRanking1() {
+		init();
 		this.leaderboard.addToRanking("Test1", 5);
 //		this.leaderboard.save();
 //		this.leaderboard.load();
@@ -36,6 +36,7 @@ public class LeaderboardTest {
 	
 	@Test
 	public void testAddToRanking2() {
+		init();
 		this.leaderboard.addToRanking("Test2", 10);
 		this.list.add(0, new Pair<>("Test2", 10));
 		assertEquals(this.list, this.leaderboard.getRanking());
@@ -43,18 +44,35 @@ public class LeaderboardTest {
 	
 	@Test
 	public void testAddToRanking3() {
+		init();
 	    this.leaderboard.addToRanking("Test3", 1);
 	    this.list.add(new Pair<>("Test3", 1));
 	    assertEquals(this.list, this.leaderboard.getRanking());
 	}
-	
+	*/
 	@Test
-	public void testGetRanking() {
-	    assertEquals(this.list, this.leaderboard.getRanking());
+	public void testAddToRanking() {
+		init();
+		
+	    assertEquals(List.of(), this.leaderboard.getRanking());
+	    
+	    this.leaderboard.addToRanking("Test1", 5);
+	    this.leaderboard.addToRanking("Test2", 10);
+	    this.leaderboard.addToRanking("Test3", 1);
+	    
+	    List<Pair<String, Integer>> control = List.of(new Pair<>("Test2", 10),new Pair<>("Test1", 5),new Pair<>("Test3", 1));
+	   
+	    assertEquals(control, this.leaderboard.getRanking());
 	}
 	
 	@Test
 	public void testGetRank() {
+		init();
+		
+		this.leaderboard.addToRanking("Test1", 5);
+	    this.leaderboard.addToRanking("Test2", 10);
+	    this.leaderboard.addToRanking("Test3", 1);
+	    
 	    assertEquals(1, this.leaderboard.getRank("Test2", 10));
 	    assertEquals(2, this.leaderboard.getRank("Test1", 5));
 	    assertEquals(3, this.leaderboard.getRank("Test3", 1));
